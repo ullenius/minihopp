019be4c577829cea5f0453a17d78f87241787f5e
Anosh D. Ullenius <anosh@anosh.se>
2019-02-19 12:55:27 +0100

    Implementing CRUD. Only 2 methods left

--- src/main/java/se/anosh/minihopp/dataaccess/RedisShortURL.java
+++ src/main/java/se/anosh/minihopp/dataaccess/RedisShortURL.java
@@ -41,28 +41,45 @@ public class RedisShortURL implements ShortURLDataAccess {
     private void switchDb(Jedis jedis) {
         jedis.select(databaseId);
     }
-    
 
     @Override
     public void remove(int id) {
         
         try (Jedis jedis = new Jedis(hostname)) {
             switchDb(jedis);
-            
             String key = String.valueOf(id);
             jedis.del(key);
-            
         }
     }
 
     @Override
     public void add(ShortURL newURL) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        String url = newURL.getOriginal();
+        int rawKey = newURL.getPath();
+        String key = String.valueOf(rawKey);
+        
+        try (Jedis jedis = new Jedis(hostname)) {
+            switchDb(jedis);
+            jedis.set(key, url);
+        }
+        
     }
 
     @Override
     public ShortURL findbyId(int id) throws ShortURLNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        String key = String.valueOf(id);
+        
+        try (Jedis jedis = new Jedis(hostname)) {
+            switchDb(jedis);
+            
+            if (!jedis.exists(key))
+                throw new ShortURLNotFoundException("no matching short url found in Redis DB");
+            
+            String value = jedis.get(key);
+            return new ShortURL(id,value);
+        }
     }
 
     @Override
